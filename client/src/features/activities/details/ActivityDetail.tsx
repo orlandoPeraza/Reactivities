5275639fbcd2d8d79b4fff2fb542bba5c8d212b0
OrlandoPeraza <peraza.oj@gmail.com>
2025-11-14 18:21:43 +0100

    Enable viewing details of a selected activity

--- client/src/features/activities/details/ActivityDetail.tsx
+++ client/src/features/activities/details/ActivityDetail.tsx
@@ -9,9 +9,13 @@ import {
 
 type Props = {
   activity: Activity;
+  cancelSelectActivity: () => void;
 };
 
-export default function ActivityDetail({ activity }: Props) {
+export default function ActivityDetail({
+  activity,
+  cancelSelectActivity,
+}: Props) {
   return (
     <Card sx={{ borderRadius: 3 }}>
       <CardMedia
@@ -27,7 +31,9 @@ export default function ActivityDetail({ activity }: Props) {
       </CardContent>
       <CardActions>
         <Button color="primary">Edit</Button>
-        <Button color="inherit">Cancel</Button>
+        <Button onClick={cancelSelectActivity} color="inherit">
+          Cancel
+        </Button>
       </CardActions>
     </Card>
   );
