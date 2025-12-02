69bd8d06eba0a63bad080e79fbbfedcdcf0d4438
OrlandoPeraza <peraza.oj@gmail.com>
2025-12-02 17:49:57 +0100

    Set ActivityForm title based on route mode using key prop

--- client/src/features/activities/form/ActivityForm.tsx
+++ client/src/features/activities/form/ActivityForm.tsx
@@ -37,7 +37,7 @@ export default function ActivityForm() {
   return (
     <Paper sx={{ borderRadius: 3, padding: 3 }}>
       <Typography variant="h5" gutterBottom color="primary">
-        Create Activity
+        {activity ? "Edit Activity" : "Create Activity"}
       </Typography>
       <Box
         component="form"
