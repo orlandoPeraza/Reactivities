fa1409cc8bfb1668aef1799f189638e92576d80a
OrlandoPeraza <peraza.oj@gmail.com>
2025-12-07 11:36:42 +0100

    Configure staleTime to optimize automatic refetching behavior

--- client/src/lib/hooks/useActivities.ts
+++ client/src/lib/hooks/useActivities.ts
@@ -9,6 +9,7 @@ export const useActivities = (id?: string) => {
       const response = await agent.get<Activity[]>("/activities");
       return response.data;
     },
+    staleTime: 1000 * 60 * 5,
   });
 
   const { data: activity, isLoading: isLoadingActivity } = useQuery({
