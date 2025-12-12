c7a727f77aa304234b3a35442d042869dad06c2f
OrlandoPeraza <peraza.oj@gmail.com>
2025-12-12 06:09:40 +0100

    Enhance axios error handling with navigation and toast notifications

--- client/src/lib/api/agent.ts
+++ client/src/lib/api/agent.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 import { store } from "../stores/store";
+import { toast } from "react-toastify";
+import { router } from "../../app/router/Routes";
 
 const sleep = (delay: number) => {
   return new Promise((resolve) => {
@@ -30,6 +32,34 @@ agent.interceptors.response.use(
   },
   (error) => {
     store.uiStore.stopLoading();
+    const { status, data } = error.response;
+    switch (status) {
+      case 400:
+        if (data.errors) {
+          const modalStateErrors = [];
+          for (const key in data.errors) {
+            if (data.errors[key]) {
+              modalStateErrors.push(data.errors[key]);
+            }
+          }
+          throw modalStateErrors.flat();
+        } else {
+          toast.error(data);
+        }
+        break;
+      case 401:
+        toast.error("Unauthorised");
+        break;
+      case 404:
+        router.navigate("/not-found");
+        break;
+      case 500:
+        router.navigate("/server-error", { state: { error: data } });
+        break;
+      default:
+        break;
+    }
+
     return Promise.reject(error);
   }
 );
