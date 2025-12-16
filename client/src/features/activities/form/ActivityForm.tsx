35faf515db9e37bb062d409a13a625783d5e2609
OrlandoPeraza <peraza.oj@gmail.com>
2025-12-16 06:22:17 +0100

    Update ActivityForm to use location object and LocationInput

--- client/src/features/activities/form/ActivityForm.tsx
+++ client/src/features/activities/form/ActivityForm.tsx
@@ -12,6 +12,7 @@ import TextInput from "../../../app/shared/components/TextInput";
 import SelectInput from "../../../app/shared/components/SelectInput";
 import { categoryOptions } from "./CategoryOptions";
 import DateTimeInput from "../../../app/shared/components/DateTimeInput";
+import LocationInput from "../../../app/shared/components/LocationInput";
 
 export default function ActivityForm() {
   const { control, reset, handleSubmit } = useForm<ActivitySchema>({
@@ -22,8 +23,6 @@ export default function ActivityForm() {
       description: "",
       category: "",
       date: new Date(),
-      city: "",
-      venue: "",
     },
   });
   const { id } = useParams();
@@ -71,8 +70,11 @@ export default function ActivityForm() {
           name="category"
         />
         <DateTimeInput label="Date" control={control} name="date" />
-        <TextInput label="City" control={control} name="city" />
-        <TextInput label="Venue" control={control} name="venue" />
+        <LocationInput
+          control={control}
+          label="Enter the location"
+          name="location"
+        />
         <Box display="flex" justifyContent="end" gap={3}>
           <Button color="inherit">Cancel</Button>
           <Button
