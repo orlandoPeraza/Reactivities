66f4e4a991559295e074b2d0d561a6a6c9b35a2c
OrlandoPeraza <peraza.oj@gmail.com>
2025-12-22 19:58:27 +0100

    Fetch current user and sync UI after login

--- client/src/lib/hooks/useAccount.ts
+++ client/src/lib/hooks/useAccount.ts
@@ -1,14 +1,31 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import agent from "../api/agent";
 import type { LoginSchema } from "../schemas/loginSchema";
 
 export const useAccount = () => {
+  const queryClient = useQueryClient();
+
   const loginUser = useMutation({
     mutationFn: async (creds: LoginSchema) => {
       await agent.post("/login?useCookies=true", creds);
     },
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({
+        queryKey: ["user"],
+      });
+    },
   });
+
+  const { data: currentUser } = useQuery({
+    queryKey: ["user"],
+    queryFn: async () => {
+      const response = await agent.get<User>("/account/user-info");
+      return response.data;
+    },
+  });
+
   return {
     loginUser,
+    currentUser,
   };
 };
