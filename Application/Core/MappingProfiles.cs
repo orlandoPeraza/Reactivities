2a5a65ef4d0fee1d4edcaa970dd738e30cbfafa0
OrlandoPeraza <peraza.oj@gmail.com>
2025-12-26 05:35:42 +0100

    Return ActivityDto instead of domain entity in activity details

--- Application/Core/MappingProfiles.cs
+++ Application/Core/MappingProfiles.cs
@@ -1,5 +1,6 @@
 using System;
 using Application.Activities.DTOs;
+using Application.Profiles.DTOs;
 using AutoMapper;
 using Domain;
 
@@ -14,6 +15,16 @@ public class MappingProfiles : Profile
         CreateMap<EditActivityDto, Activity>()
         .ForAllMembers(opt => 
         opt.Condition((src, dest, srcMember) => srcMember != null));
+        CreateMap<Activity, ActivityDto>()
+        .ForMember(d => d.HostDisplayName, o => o.MapFrom(s => 
+            s.Attendees.FirstOrDefault(x => x.IsHost)!.User.DisplayName))
+        .ForMember(d => d.HostId, o => o.MapFrom(s => 
+            s.Attendees.FirstOrDefault(x => x.IsHost)!.User.Id));
+        CreateMap<ActivityAttendee, UserProfileDto>()
+        .ForMember(d => d.DisplayName, o => o.MapFrom(s =>s.User.DisplayName))
+        .ForMember(d => d.Bio, o => o.MapFrom(s =>s.User.Bio))
+        .ForMember(d => d.ImageUrl, o => o.MapFrom(s =>s.User.ImageUrl))
+        .ForMember(d => d.Id, o => o.MapFrom(s =>s.User.Id));        
     }
 
 }
