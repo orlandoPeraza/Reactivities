a9604819f1cca9121fccf913be3b82bd75aebf53
OrlandoPeraza <peraza.oj@gmail.com>
2025-12-29 18:17:40 +0100

    Show profile preview on attendee avatar hover

--- client/src/features/activities/dashboard/ActivityCard.tsx
+++ client/src/features/activities/dashboard/ActivityCard.tsx
@@ -13,6 +13,7 @@ import {
 import { Link } from "react-router";
 import { formatDate } from "../../../lib/util/util";
 import type { Activity } from "../../../lib/types";
+import AvatarPopover from "../../../app/shared/components/AvatarPopover";
 type Props = {
   activity: Activity;
 };
@@ -41,7 +42,12 @@ export default function ActivityCard({ activity }: Props) {
         />
         <Box display="flex" flexDirection="column" gap={2} mr={2}>
           {(activity.isHost || activity.isGoing) && (
-            <Chip label={label} color={color} sx={{ borderRadius: 2 }} />
+            <Chip
+              variant="outlined"
+              label={label}
+              color={color}
+              sx={{ borderRadius: 2 }}
+            />
           )}
           {activity.isCancelled && (
             <Chip label="Cancelled" color="error" sx={{ borderRadius: 2 }} />
@@ -68,13 +74,7 @@ export default function ActivityCard({ activity }: Props) {
           sx={{ backgroundColor: "grey.200", py: 3, pl: 3 }}
         >
           {activity.attendees.map((att) => (
-            <Avatar
-              key={att.id}
-              alt={att.displayName + " iamge"}
-              src={att.imageUrl}
-              component={Link}
-              to={`/profiles/${att.id}`}
-            />
+            <AvatarPopover profile={att} key={att.id} />
           ))}
         </Box>
       </CardContent>
