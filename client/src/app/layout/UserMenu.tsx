6dbe9ba7e741b7e34e8f96255f908d5d5d741b83
OrlandoPeraza <peraza.oj@gmail.com>
2026-01-05 15:33:02 +0100

    Add user profile page and navigation

--- client/src/app/layout/UserMenu.tsx
+++ client/src/app/layout/UserMenu.tsx
@@ -34,7 +34,7 @@ export default function UserMenu() {
         sx={{ fontSize: "1.1rem" }}
       >
         <Box display="flex" alignItems="center" gap={2}>
-          <Avatar />
+          <Avatar src={currentUser?.imageUrl} alt="Image of host" />
           {currentUser?.displayName}
         </Box>
       </Button>
@@ -53,7 +53,11 @@ export default function UserMenu() {
           </ListItemIcon>
           <ListItemText>Create Activity</ListItemText>
         </MenuItem>
-        <MenuItem component={Link} to="/profile" onClick={handleClose}>
+        <MenuItem
+          component={Link}
+          to={`/profiles/${currentUser?.id}`}
+          onClick={handleClose}
+        >
           <ListItemIcon>
             <Person />
           </ListItemIcon>
