4aaea141cbd55a3de4ac7d1ccdaf20680c4b8a30
OrlandoPeraza <peraza.oj@gmail.com>
2026-01-25 19:35:31 +0100

    Make navbar fixed and activity filters sticky

--- client/src/app/layout/NavBar.tsx
+++ client/src/app/layout/NavBar.tsx
@@ -6,7 +6,7 @@ import {
   Container,
   MenuItem,
   Typography,
-  LinearProgress,
+  CircularProgress,
 } from "@mui/material";
 import { NavLink } from "react-router";
 import MenuItemLink from "../shared/components/MenuItemLink";
@@ -21,11 +21,10 @@ export default function NavBar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar
-        position="static"
+        position="fixed"
         sx={{
           backgroundImage:
             "linear-gradient(135deg, #182a73 0%, #218aae 69%, #20a7ac 89%)",
-          position: "relative",
         }}
       >
         <Container maxWidth="xl">
@@ -37,9 +36,29 @@ export default function NavBar() {
                 sx={{ display: "flex", gap: 2 }}
               >
                 <Group fontSize="large" />
-                <Typography variant="h4" fontWeight="bold">
+                <Typography
+                  sx={{ position: "relative" }}
+                  variant="h4"
+                  fontWeight="bold"
+                >
                   Reactivities
                 </Typography>
+                <Observer>
+                  {() =>
+                    uiStore.isLoading ? (
+                      <CircularProgress
+                        size={20}
+                        thickness={7}
+                        sx={{
+                          color: "white",
+                          position: "absolute",
+                          top: "30%",
+                          left: "105%",
+                        }}
+                      />
+                    ) : null
+                  }
+                </Observer>
               </MenuItem>
             </Box>
             <Box sx={{ display: "flex" }}>
@@ -59,23 +78,6 @@ export default function NavBar() {
             </Box>
           </Toolbar>
         </Container>
-
-        <Observer>
-          {() =>
-            uiStore.isLoading ? (
-              <LinearProgress
-                color="secondary"
-                sx={{
-                  position: "absolute",
-                  bottom: 0,
-                  left: 0,
-                  right: 0,
-                  height: 4,
-                }}
-              />
-            ) : null
-          }
-        </Observer>
       </AppBar>
     </Box>
   );
