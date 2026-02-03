3b79fe2d5e90f841fc99231424e71490764081bf
OrlandoPeraza <peraza.oj@gmail.com>
2026-02-03 12:16:06 +0100

    Configure Vite to output client build into API wwwroot

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import mkcert from "vite-plugin-mkcert";
-// https://vite.dev/config/
+
 export default defineConfig({
+  base: "/",
+  build: {
+    outDir: "../API/wwwroot",
+    emptyOutDir: true,
+    chunkSizeWarningLimit: 1500,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
   server: {
     port: 3000,
   },
